ef6eafc10cb8ff1c0d883e0572f87f037cad034d
Sabin Adhikari <binsone001@gmail.com>
2026-07-05 19:09:23 +0545

    Refactor weather data fetching logic and update humidity display

--- logic.js
+++ logic.js
@@ -1,3 +1,20 @@
 const apiKey = "fac3e37512cde1c071565b6aab26d7a4";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=kathmandu";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&deg;C";
+    document.querySelector(".Humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".Wind").innerHTML = data.wind.speed + " Km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})

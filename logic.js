66947099d6a47ae9bd5f0eefaf1c6fbd953b82a8
Sabin Adhikari <binsone001@gmail.com>
2026-07-05 19:55:23 +0545

    Add error handling for invalid city names and improve weather icon styling

--- logic.js
+++ logic.js
@@ -2,10 +2,16 @@ const apiKey = "fac3e37512cde1c071565b6aab26d7a4";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const WeatherIcon = document.querySelector(".Weather-Icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+            var data = await response.json();
 
     console.log(data);
 
@@ -14,8 +20,30 @@ async function checkWeather(city) {
     document.querySelector(".Humidity").innerHTML = data.main.humidity + " %";
     document.querySelector(".Wind").innerHTML = data.wind.speed + " Km/h";
 
+    if (data.weather[0].main == "Clouds") {
+        WeatherIcon.src = "images/cloudy.png"
 
-    
+
+    }else if(data.weather[0].main == "Clear") {
+        WeatherIcon.src = "images/clear_sun.png"
+
+
+    }else if(data.weather[0].main == "Rain") {
+        WeatherIcon.src = "images/Rain.png"
+
+
+    }else if(data.weather[0].main == "Drizzle") {
+        WeatherIcon.src = "images/Drizzle.png"
+
+
+    }else if(data.weather[0].main == "Mist") {
+        WeatherIcon.src = "images/mist.png"
+    }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+
 }
 
 searchBtn.addEventListener("click", () => {
